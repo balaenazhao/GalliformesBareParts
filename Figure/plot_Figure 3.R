bf2221a3a9d1905b8ebd916142f51ba50e205443
balaenazhao <42870954+balaenazhao@users.noreply.github.com>
2023-11-29 15:21:25 -0500

    Update plot_Figure 3.R

--- Figure/plot_Figure 3.R
+++ Figure/plot_Figure 3.R
@@ -4,7 +4,6 @@ library(magrittr)       # for easier syntax in one or two areas
 library(gridExtra)      # for generating the bin width comparison plot
 library(ggplot2)
 
-setwd("/Users/balaena/Downloads/postdoc/GalliformesFS/manuscript/Figures/Figure 3 - phas vs non-phas/")
 data<- read.csv(file="data_for_fig3.csv",header=TRUE)
 
 data_mean <- data %>%
@@ -17,36 +16,3 @@ ggplot(data, aes(x = score, fill = as.factor(group))) +
   geom_vline(data = data_mean, aes(xintercept = Mean), linetype = "dashed") +
   facet_grid(group ~ size,scales = "free") +
   theme_bw()
-
-
-
-
-#### violin plots 
-p <- ggplot(data, aes(x=group, y=flaccidFS)) + 
-  geom_violin() + stat_summary(fun.data=data_summary)
-p2 <- ggplot(data, aes(x=group, y=Female_FS)) + 
-  geom_violin() + stat_summary(fun.data=data_summary)
-
-data_summary <- function(x) {
-  m <- mean(x)
-  ymin <- m-sd(x)
-  ymax <- m+sd(x)
-  return(c(y=m,ymin=ymin,ymax=ymax))
-}
-
-#### single grid of histogram
-
-p<- ggplot(data[1:104,], aes(x=flaccidFS)) +
-  geom_histogram(fill="white", alpha=0.5, position="identity")
-
-p + geom_vline(aes(xintercept=mean(flaccidFS)),
-               color="blue", linetype="dashed", size=1)
-
-p1<- ggplot(data[105:264,], aes(x=flaccidFS)) +
-  geom_histogram(fill="white", alpha=0.5, position="identity")
-
-p1 + geom_vline(aes(xintercept=mean(flaccidFS)),
-                color="blue", linetype="dashed", size=1)
-
-
-
